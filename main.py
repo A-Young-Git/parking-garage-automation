18384de71f7b60836582c860367c05885e8e2ff3
Tony <tony3159292@gmail.com>
2025-08-30 14:52:39 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+
+"""
+- The parking garage has 8 floors and 20 spaces per floor
+- The customer is expected to go to the floor they are assigned 
+- We start on the first floor, and work upwards. 
+- As spaces open up on lower floors, direct new customers there. 
+- Two functions, entry() and exit() will service these customers.
+"""
+
+garage = {"1": 20, "2": 20, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0}
+
+Ticket = namedtuple("Ticket", ["occupant_number", "garage_floor"])
+
+active_tickets = []
+
+
+def enter() -> str | None:
+    for floor in garage:
+        if garage[floor] > 0:
+            garage[floor] -= 1
+            return floor
+    return None
+
+
+def exit():
+    ticket_number = input("Please provide your ticket number and press 'Enter': ")
+
+    for ticket in active_tickets:
+        if ticket_number == ticket.occupant_number:
+            active_tickets.remove(ticket)
+            break
+        else:
+            print(
+                "Ticket number not found. Please check you entered it correctly and try again."
+            )
+
+
+if __name__ == "__main__":
+    input("Welcome to T's Garage Services! Press 'Enter' for a number: ")
+
+    floor = enter()
+
+    if floor is not None:
+        ticket = Ticket(occupant_number=1, garage_floor=floor)
+        active_tickets.append(ticket)
+        print(
+            f"You have ticket number {ticket.occupant_number}. Please proceed to floor {ticket.garage_floor}"
+        )
+    else:
+        print("Sorry. We are at full capacity. Please try again later.")
